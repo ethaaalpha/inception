a7f62f3e70c3f2ccce590caf32ac9b4b3a72951b
ethaaalpha <revenus-nager-0s@icloud.com>
2023-12-10 22:21:59 +0100

    add basic user

--- srcs/requirements/wordpress/conf/config.sh
+++ srcs/requirements/wordpress/conf/config.sh
@@ -8,5 +8,7 @@ wp db create --allow-root
 
 wp core --allow-root install 	--url=$DOMAIN --title=$WP_TITLE \
 --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_MAIL
-								
+
+wp user create --allow-root $WP_BASIC_USER $WP_BASIC_MAIL --user_pass=$WP_BASIC_PASSWORD
+
 php-fpm7.4 -F -R
